ed5468f2454bef9519c678468ecdb9a6cf2fd4ec
MayaRozenberg <maya.rozen53@gmail.com>
2025-04-26 18:38:31 +0300

    Implement Routing, Dashboard Layout & UI

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Header } from "components";
 
 const AllUsers = () => {
-  return <div>Users Table</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips page"
+        description="Check out our current users in real time"
+      />
+      All Users Page contents
+    </main>
+  );
 };
 
 export default AllUsers;
